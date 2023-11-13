84e1c445eb97332fbc39d5c94e6ded59a248aff4
564522as <126795390+564522as@users.noreply.github.com>
2023-11-13 09:47:01 +0300

    Update Table4x4Tests.java

--- src/test/java/com/electronic/tables/table/Table4x4Tests.java
+++ src/test/java/com/electronic/tables/table/Table4x4Tests.java
@@ -1,2 +1,16 @@
-package com.electronic.tables.table;public class Table4x4Tests {
+package com.electronic.tables.table;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+public class Table4x4Tests {
+    private Table4x4 table = new Table4x4();
+    @Test
+    void findCellTest() {
+        Optional<Cell> cell = table.findCell("A1");
+        Assertions.assertTrue(cell.isPresent());
+    }
 }
+
