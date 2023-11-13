354220bd09a18584dcb0fff0bb7073e94ba7f2dd
564522as <126795390+564522as@users.noreply.github.com>
2023-11-13 09:41:59 +0300

    Update Table4x4.java

--- src/main/java/com/electronic/tables/table/Table4x4.java
+++ src/main/java/com/electronic/tables/table/Table4x4.java
@@ -1,2 +1,18 @@
-package com.electronic.tables.table;public class Table4x4 {
+package com.electronic.tables.table;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Table4x4 extends Table{
+    @Override
+    public void fillTable() {
+        String[] columns = {"A", "B", "C", "D"};
+        String[] rows = {"1", "2", "3", "4"};
+        for (String column: columns) {
+            for (String row: rows) {
+                addCell(new Cell("", column+row));
+            }
+        }
+    }
 }
