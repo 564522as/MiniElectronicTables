5b99f866671acc35eabaeab876831a7f7d64e146
564522as <126795390+564522as@users.noreply.github.com>
2023-11-13 09:42:38 +0300

    Update Cell.java

--- src/main/java/com/electronic/tables/table/Cell.java
+++ src/main/java/com/electronic/tables/table/Cell.java
@@ -1,2 +1,28 @@
-package com.electronic.tables.table;public class Cell {
+package com.electronic.tables.table;
+
+public class Cell {
+    private String value;
+    private String address;
+
+    public Cell(String value, String address) {
+        this.value = value;
+        this.address = address;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
+
