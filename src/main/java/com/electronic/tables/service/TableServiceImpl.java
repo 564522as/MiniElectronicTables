d23c1c89da8656803f729b59f98a5a4574aae29c
564522as <126795390+564522as@users.noreply.github.com>
2023-11-13 09:43:45 +0300

    Update TableServiceImpl.java

--- src/main/java/com/electronic/tables/service/TableServiceImpl.java
+++ src/main/java/com/electronic/tables/service/TableServiceImpl.java
@@ -1,2 +1,33 @@
-package com.electronic.tables.service;public class TableServiceImpl {
+package com.electronic.tables.service;
+
+import com.electronic.tables.math.FormulaProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TableServiceImpl implements TableService{
+    private FormulaProcessor processor;
+
+    @Autowired
+    public TableServiceImpl(FormulaProcessor processor) {
+        this.processor = processor;
+    }
+
+
+    @Override
+    public String processValue(String value) {
+        if (!value.isEmpty() && value.charAt(0) == '=') {
+            try {
+                return processor.calculateFormula(value.substring(1));
+            } catch (Exception e) {
+                return "";
+            }
+        } else {
+            try {
+                return String.valueOf(Double.parseDouble(value));
+            } catch (NumberFormatException e) {
+                return "";
+            }
+        }
+    }
 }
